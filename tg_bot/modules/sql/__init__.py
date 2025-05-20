c69e375d7213db4d86d136c62f4ff22108b12658
DaGreatHost <heherson230@gmail.com>
2025-05-21 02:42:05 +0800

    Update __init__.py

--- tg_bot/modules/sql/__init__.py
+++ tg_bot/modules/sql/__init__.py
@@ -1,16 +1,24 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from tg_bot import DB_URI
+from tg_bot import Config
 
+def start():
+    db_url = Config.SQLALCHEMY_DATABASE_URI
+    if not db_url:
+        print("No database URL configured - Database functionality disabled")
+        return None
 
-def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    engine = create_engine(db_url, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
-
 
 BASE = declarative_base()
 SESSION = start()
