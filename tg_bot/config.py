4fe4d17525eac9312a02cb5d3191d1a083192cc3
DaGreatHost <heherson230@gmail.com>
2025-05-21 02:35:54 +0800

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -1,44 +1,23 @@
-if not __name__.endswith("sample_config"):
-    import sys
-    print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
-          "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
-    quit(1)
-
-
-# Create a new config.py file in same dir and import, then extend this class.
-class Config(object):
-    LOGGER = True
-
-    # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
-
-    # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
-    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
-    LOAD = []
-    NO_LOAD = ['translation', 'rss']
-    WEBHOOK = False
-    URL = None
-
-    # OPTIONAL
-    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    DONATION_LINK = None  # EG, paypal
-    CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
-    STRICT_GBAN = False
-    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
-    ALLOW_EXCL = False  # Allow ! commands as well as /
-    BMERNU_SCUT_SRELFTI = 0
-
-class Production(Config):
-    LOGGER = False
-
+from tg_bot.sample_config import Config
 
 class Development(Config):
-    LOGGER = True
+    OWNER_ID = int(os.environ.get('OWNER_ID'))  # Your Telegram ID
+    OWNER_USERNAME = os.environ.get('OWNER_USERNAME')  # Your Telegram username
+    API_KEY = os.environ.get('TOKEN')  # Bot token from @BotFather
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # Database URL from Railway
+    MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)  # Channel/Group ID for logging
+    LOAD = []  # Load these modules
+    NO_LOAD = ['translation']  # Don't load these modules
+    WEBHOOK = bool(os.environ.get('WEBHOOK', False))
+    URL = os.environ.get('URL', "")  # Your Railway app URL
+
+    # Optional
+    SUDO_USERS = []  # List of ID's - (not usernames)
+    SUPPORT_USERS = []  # List of ID's - (not usernames)
+    WHITELIST_USERS = []  # List of ID's - (not usernames)
+    CERT_PATH = None
+    PORT = int(os.environ.get('PORT', 5000))
+    DEL_CMDS = bool(os.environ.get('DEL_CMDS', True))
+    STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', True))
+    WORKERS = int(os.environ.get('WORKERS', 8))
+    ALLOW_EXCL = bool(os.environ.get('ALLOW_EXCL', True))
